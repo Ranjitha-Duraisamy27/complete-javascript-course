21582cd5dcc9fa14bdff90d424018d6658661a5f
Ranjitha-Duraisamy27 <ranjitha.duraisamy27@gmail.com>
2022-09-13 20:53:24 +0530

    close and open modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const showModalBtn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < showModalBtn.length; i++) {
+    showModalBtn[i].addEventListener('click', openModal);
+}
+
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
