1ffb8ea04dea4d2040b75f0e30965c381ef11cb0
Ranjitha-Duraisamy27 <ranjitha.duraisamy27@gmail.com>
2022-09-18 11:12:55 +0530

    destructuring coding challenge

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+/**
+ * Scoping in Practice
+ */
+
+function callAge(birthYear) {
+    var birthYear = 2037 - birthYear;
+    console.log(birthYear);
+}
+
+callAge(1994);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,87 @@ const restaurant = {
     },
   },
 };
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+/**
+ * Coding Challenge 1
+ */
+
+//Task 1
+const [players1, players2] = game.players;
+console.log('Players1 => ', players1);
+console.log('Players2 => ', players2);
+
+//Task 2
+const [team1Gk, ...team1FieldPlayers] = players1; 
+const [team2Gk, ...team2FieldPlayers] = players2;
+console.log(`For team 1 ${team1Gk} is the goal keeper and ${team1FieldPlayers} are the field players`);
+console.log(`For team 2 ${team2Gk} is the goal keeper and ${team2FieldPlayers} are the field players`);
+
+//Task 3
+const allPlayers = [...players1, ...players2];
+console.log('All Players : ', allPlayers);
+
+//Task 4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log('Final players list of team 1 : ', players1Final);
+
+//Task 5
+const { team1, x: draw, team2 } = game.odds;
+console.log('Team 1 Odds : ', team1);
+console.log('Draw : ', draw);
+console.log('Team 2 odds : ', team2);
+
+//Task 6
+const printGoals = (...players) => {
+  console.log(`Players: ${players}, Total Score: ${players.length}`);
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+//Task 7
+
+(team1 > team2) && console.log('Team 1 is more likely to win');
+(team2 > team1) && console.log('Team 2 is more likely to win');
+
